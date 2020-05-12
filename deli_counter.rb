afa36eb9d4d073628a0153f79f7762444ea27dc0
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-12 18:44:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(number)
     puts "The line is currently empty."
   end
   
-def take_a_number(katz_deli)
+def take_a_number(katz_deli, name)
   counter = 1
   katz_deli = []
   puts "Welcome #{katz_deli.last}.  You are number #{counter} in line."
